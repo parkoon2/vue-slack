842729fd9c4404c2a1cacdd5be69f3a635a859da
parkoon <par_koon@naver.com>
2019-10-02 20:24:00 +0900

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Hello world</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
