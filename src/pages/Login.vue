c5c42e6bef3a7de68557d9d8853b5a4a7827105f
parkoon <par_koon@naver.com>
2019-10-02 20:32:52 +0900

    5. Setup login page with bootstrap

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,23 @@
 <template>
-  <h2>login page</h2>
+  <div>
+    <div class="jumbotron bg-primary text-white text-center">
+      <h2 class="lead display-3">#SLACK#</h2>
+      <p>Realtime communication at it's best</p>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col text-center">
+          <button class="btn btn-outline-danger btn-lg">Login with Google</button>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col text-center">
+          <button class="btn btn-outline-info btn-lg">Login with Twitter</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +25,10 @@ export default {
   name: "login"
 };
 </script>
+
+<style>
+.btn,
+.jumbotron {
+  border-radius: 0px;
+}
+</style>
