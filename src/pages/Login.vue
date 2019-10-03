e5189f66191b210e3dce98f5f7f8c219b964a5e1
parkoon <par_koon@naver.com>
2019-10-03 12:29:04 +0900

    Login with google

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -8,7 +8,7 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col text-center">
-          <button class="btn btn-outline-danger btn-lg">Login with Google</button>
+          <button @click="loginWithGoogle" class="btn btn-outline-danger btn-lg">Login with Google</button>
         </div>
       </div>
       <div class="row mt-5">
@@ -21,8 +21,21 @@
 </template>
 
 <script>
+import auth from "firebase/auth";
 export default {
-  name: "login"
+  name: "login",
+
+  methods: {
+    loginWithGoogle() {
+      console.log("login with google", firebase);
+      firebase
+        .auth()
+        .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+        .then(response => {
+          console.log(response);
+        });
+    }
+  }
 };
 </script>
 
