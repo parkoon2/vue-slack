aeaeb170fafe5d51074301eb6a2f2b9d9958a808
parkoon <par_koon@naver.com>
2019-10-03 12:19:08 +0900

    Setting: Firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,27 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase/app'
 
 Vue.config.productionTip = false
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyADwsnV6dae6QcKy8vLfo_cRp1Yhk-Y8Uw",
+  authDomain: "vuexslack-fcdbb.firebaseapp.com",
+  databaseURL: "https://vuexslack-fcdbb.firebaseio.com",
+  projectId: "vuexslack-fcdbb",
+  storageBucket: "",
+  messagingSenderId: "209602853775",
+  appId: "1:209602853775:web:024a4a6735ec36f29469fa",
+  measurementId: "G-7ECG2B1MLT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+window.firebase = firebase;
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +32,5 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+
