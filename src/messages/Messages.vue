28da2d30a62af52fa9923829f6f8215e0fd293c9
parkoon <par_koon@naver.com>
2019-10-04 12:53:32 +0900

    Getting messages

--- src/messages/Messages.vue
+++ src/messages/Messages.vue
@@ -11,6 +11,7 @@
 import SingleMessage from "./SingleMessage";
 import MessageForm from "./MessageForm";
 import database from "firebase/database";
+import { mapGetters } from "vuex";
 
 export default {
   name: "message",
@@ -20,8 +21,41 @@ export default {
   },
   data() {
     return {
-      messageRef: firebase.database().ref("messages")
+      messageRef: firebase.database().ref("messages"),
+      messages: [],
+      channel: ""
     };
+  },
+  computed: {
+    ...mapGetters(["currentChannel"])
+  },
+
+  watch: {
+    currentChannel: function() {
+      // if current channel changes, watch for its messages
+      this.messages = [];
+      this.addListeners();
+      this.channel = this.currentChannel;
+    }
+  },
+  methods: {
+    addListeners() {
+      // listen to child added events on current channel
+      this.messageRef
+        .child(this.currentChannel.id)
+        .on("child_added", snapshot => {
+          this.messages.push(snapshot.val());
+        });
+    },
+
+    detachListeners() {
+      if (this.channel) {
+        this.messageRef.child(this.channel.id).off();
+      }
+    }
+  },
+  beforeDistory() {
+    this.detachListeners();
   }
 };
 </script>
