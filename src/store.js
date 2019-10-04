312d9b7e0d7961df01f630c03302e49dcd074dae
parkoon <par_koon@naver.com>
2019-10-04 10:12:59 +0900

    Set current channel in store

--- src/store.js
+++ src/store.js
@@ -1,38 +1,46 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 // Create state that holds data
 const state = {
-    currentUser: null
-}
+  currentUser: null,
+  currentChannel: null
+};
 
 // Update state data through mutations
 const mutations = {
-    SET_USER(state, payload) {
-        state.currentUser = payload
-    }
-}
+  SET_USER(state, payload) {
+    state.currentUser = payload;
+  },
+  SET_CURRENT_CHANNEL(state, channel) {
+    state.currentChannel = channel;
+  }
+};
 
 // Create actions for mutations with necessary data
 const actions = {
-    setUser({ commit }, payload) {
-        commit('SET_USER', payload)
-    }
-}
+  setUser({ commit }, payload) {
+    commit("SET_USER", payload);
+  },
+
+  setCurrentChannel({ commit }, channel) {
+    commit("SET_CURRENT_CHANNEL", channel);
+  }
+};
 
 // Get data from state
 const getters = {
-    currentUser: state => state.currentUser
-}
+  currentUser: state => state.currentUser
+};
 
 // Create vuex store
 const store = new Vuex.Store({
-    state,
-    mutations,
-    actions,
-    getters
-})
+  state,
+  mutations,
+  actions,
+  getters
+});
 
-export default store
+export default store;
