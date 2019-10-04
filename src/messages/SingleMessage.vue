9080572c00be55a86a0b778c2ffceecb5f10acb6
parkoon <par_koon@naver.com>
2019-10-04 13:03:25 +0900

    Data filter and self message styling

--- src/messages/SingleMessage.vue
+++ src/messages/SingleMessage.vue
@@ -7,9 +7,9 @@
           <div class="media-body">
             <h6 class="mt-0">
               <a href="#">{{ message.user.name}}</a>
-              - {{ message.timestamp}}
+              - {{ message.timestamp | fromNow}}
             </h6>
-            <p>{{ message.content }}</p>
+            <p :class="{'self-message': selfMessage(message.user)}">{{ message.content }}</p>
           </div>
         </div>
       </div>
@@ -17,8 +17,34 @@
   </div>
 </template>
 <script>
+import moment from "moment";
+import { mapGetters } from "vuex";
 export default {
   name: "single-message",
-  props: ["messages"]
+  props: ["messages"],
+
+  computed: {
+    ...mapGetters(["currentUser"])
+  },
+
+  methods: {
+    selfMessage(user) {
+      console.log(user, this.currentUser);
+      return user.id === this.currentUser.uid;
+    }
+  },
+
+  filters: {
+    fromNow(value) {
+      return moment(value).fromNow();
+    }
+  }
 };
 </script>
+
+<style scoped>
+.self-message {
+  border-left: 5px solid red;
+  padding: 0 10px;
+}
+</style>
